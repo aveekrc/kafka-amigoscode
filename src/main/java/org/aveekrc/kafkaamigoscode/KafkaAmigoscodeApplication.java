ac51bef91cc895b5bfb9d5c781033a5b6653929c
aveekrc <roychowdhury.aveek@gmail.com>
2022-09-20 17:30:30 +0530

    Amigos Code - Kafka

---
+++ src/main/java/org/aveekrc/kafkaamigoscode/KafkaAmigoscodeApplication.java
@@ -0,0 +1,13 @@
+package org.aveekrc.kafkaamigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAmigoscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAmigoscodeApplication.class, args);
+	}
+
+}
